44366eb8c89f6479d10580928d93f887efbc14f2
louis <farina@etud.insa-toulouse.fr>
2021-12-01 18:18:58 +0100

    day 1

---
+++ day1/day1-1.hs
@@ -0,0 +1,23 @@
+import Control.Monad
+import System.IO
+
+main = do
+	input <- readInput
+	print (increaseCount input)
+
+readInput :: IO [Int]
+readInput = do
+	line <- getLine
+	eof <- isEOF
+	if eof then return [read line]
+	else do 
+		input <- readInput 
+		return ((read line):input)
+
+increaseCount :: [Int] -> Int
+increaseCount [] = 0
+increaseCount [_] = 0
+increaseCount l = 
+	let rest = tail l in
+		(if head l < head rest then 1 else 0) + (increaseCount rest)
+
